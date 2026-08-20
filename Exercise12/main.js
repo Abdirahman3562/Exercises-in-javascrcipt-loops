6e11eeed2317f397456c8092ca7bf7d28f94daba
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-20 07:18:19 +0300

    Exercise 12

---
+++ Exercise12/main.js
@@ -0,0 +1,16 @@
+// waxa la iag rabo waa inaan soo daabaco xogta arrayda kujirto anigoo isticmaalaayo for loop
+
+
+const fruits = [
+    "Apple", "Banana", "Cherry", "Date", "Elderberry", 
+    "Fig", "Grape", "Honeydew", "Kiwi", "Lemon", 
+    "Mango", "Nectarine", "Orange", "Papaya", "Quince", 
+    "Raspberry", "Strawberry", "Tangerine", "Ugli", "Watermelon"
+];
+
+
+for (let i = 0; i < fruits.length; i++) {
+
+    console.log(fruits[i]);
+
+}
