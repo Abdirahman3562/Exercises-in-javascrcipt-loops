aca3f531cf6a46ad7530c604194499da064f8a98
Abdirahman3562 <abdirahmaanmoha2022@gmail.com>
2026-08-20 08:01:19 +0300

    Exercise 14

---
+++ Exercise14/main.js
@@ -0,0 +1,11 @@
+// waxa liga rabo wa  inaan maanta dhan userka uso bandhigo promt o aan kudhaho geli number kaweyn 10 ila u numberkas ka geliyana usan promtigas kabixin mesha anigo isticmalayo dowhile
+
+
+
+let number;
+
+do{
+    number = prompt("waxa jiro number secret ah soo saar numberkaas");
+}while(number !== "10");
+
+console.log("waad saxday numberka saxda ah waa", number);
